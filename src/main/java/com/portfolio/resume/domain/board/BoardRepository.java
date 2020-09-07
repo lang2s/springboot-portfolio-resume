aad59023725728183fb673ae6cc8d3af483dcaee
lang2s <lang2s@naver.com>
2020-09-07 22:06:44 +0900

    수정중

--- src/main/java/com/portfolio/resume/domain/board/BoardRepository.java
+++ src/main/java/com/portfolio/resume/domain/board/BoardRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    @Query("SELECT p FROM Board p ORDER BY p.id DESC")
+    @Query("SELECT b FROM Board b ORDER BY b.id DESC")
     List<Board> findAllDesc();
 
 }
